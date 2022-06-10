9d38ed10336b45e36862ffe4cba523ff9d411cd8
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-06-10 15:51:51 +0200

    fixed css fade-in, adjusted #navigation fade in

--- js/index-animate.js
+++ js/index-animate.js
@@ -1,6 +1,6 @@
 var nav = gsap.timeline({defaults: {}})
 
-nav.fromTo('#navigation', {y: 120}, {y: 0, duration: 0.5, ease: "power2.out"}, "<50%")
+nav.fromTo('#navigation', {y: 120}, {y: 0, delay: 0.2, duration: 0.5, ease: "power2.out"}, "<50%")
 
 function navOut() {
     var nav = gsap.timeline({defaults: {}})
