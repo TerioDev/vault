e66f4b162c6bb8f4aea68cdcaf648c2708aa7e3c
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-06-10 15:31:37 +0200

    changed body gsap fade in/out to CSS

--- js/index-animate.js
+++ js/index-animate.js
@@ -1,9 +1,8 @@
 var nav = gsap.timeline({defaults: {}})
 
-nav.fromTo('#body-wrapper', {opacity: 0}, {opacity: 1, duration: 1, ease: "power2.in"})
 nav.fromTo('#navigation', {y: 120}, {y: 0, duration: 0.5, ease: "power2.out"}, "<50%")
 
-function pageOut() {
-    nav.fromTo('#body-wrapper', {opacity: 1}, {opacity: 0, duration: 1, ease: "power2.out"})
+function navOut() {
+    var nav = gsap.timeline({defaults: {}})
     nav.fromTo('#navigation', {y: 0}, {y: 120, duration: 0.5, ease: "power2.in"}, "<")
 }
