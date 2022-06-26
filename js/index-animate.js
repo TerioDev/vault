e34e4fac0e3b9323d620eceaf8e37a034acba363
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-06-26 13:12:57 +0200

    index update

--- js/index-animate.js
+++
@@ -1,8 +0,0 @@
-var nav = gsap.timeline({defaults: {}})
-
-nav.fromTo('#navigation', {y: 120}, {y: 0, delay: 0.2, duration: 0.5, ease: "power2.out"}, "<50%")
-
-function navOut() {
-    var nav = gsap.timeline({defaults: {}})
-    nav.fromTo('#navigation', {y: 0}, {y: 120, duration: 0.5, ease: "power2.in"}, "<")
-}
